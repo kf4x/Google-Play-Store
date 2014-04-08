cad6b945114a3c609113393984d2951eb31d92ff
Javier C <javier@javierachavez.com>
2014-04-08 13:52:19 -0600

    added app with permissions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+[
+    "find accounts on the device",
+    "approximate location (network-based)",
+    "precise location (GPS and network-based)",
+    "full network access"
+].each do |perm|
+  Permission.find_or_create_by_name(perm)
+end
+
+
+app = App.create({searchterm: "facebook", package: "com.facebook.katana", name: "Facebook", rating: 4.3, description: 'fb'})
+
+app.permissions << Permission.find(1)
+
+comment = Comment.create({author:'rblutionkimmyb', content: 'Where\'d the notification bar go?!? What the hell Facebook after the last update it tells me i have notifications but I open the app and there\'s no way to check them please fix not very convenient when I can\'t use my phone'})
+
+app.comments << comment
