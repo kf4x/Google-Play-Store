6b324c39579a35ca12ec154f6d7a902c46e431f7
Javier C <javier@javierachavez.com>
2014-04-08 02:33:55 -0600

    related tables

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,4 +1,6 @@
 class App < ActiveRecord::Base
   attr_accessible :description, :name, :package, :rating, :searchterm
   has_and_belongs_to_many :permissions
+  has_many :images
+  has_many :comments
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,5 @@
 class Image < ActiveRecord::Base
   attr_accessible :location
+
+  belongs_to :app
 end
