ba5372a2038037356641da80959a007cb0bfb774
Javier C <javier@javierachavez.com>
2014-04-08 04:25:48 -0600

    untrack files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-GooglePlayStore::Application.config.secret_token = 'dfbc6edcb5ca3d32efd1a70844fdc77ed062ad149e6448e5b920f45269b30bf94af3382e16df06608c836f431bf8fd3d9b0d65be168dbdf3fd0b1b49f5cc3b0b'
