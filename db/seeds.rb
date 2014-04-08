84ee4481f35c57de53edba78da94aec48ca42d18
Javier C <javier@javierachavez.com>
2014-04-08 16:27:44 -0600

    add real desc

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@
 end
 
 
-app = App.create({searchterm: "facebook", package: "com.facebook.katana", name: "Facebook", rating: 4.3, description: 'fb'})
+app = App.create({searchterm: "facebook", package: "com.facebook.katana", name: "Facebook", rating: 3.9, description: 'Keeping up with friends is faster than ever.\nSee what friends are up to\nShare updates, photos and videos\net notified when friends like and comment on your posts\nText, chat and have group conversations\nPlay games and use your favorite apps\nNow you can get early access to the next version of Facebook for Android by becoming a beta tester. Learn how to sign up, givefeedback and leave the program in our Help Center: http://on.fb.me/133NwuP\nProblems downloading or installing the app? See http://bit.ly/GPDownload1\nStill need help? Please tell us more about the issue. http://bit.ly/invalidpackage\nFacebook is only available for users age 13 and over.\nTerms of Service: http://m.facebook.com/terms.php.'})
 
 app.permissions << Permission.find(1)
 
