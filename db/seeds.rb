95f54e718dc5c503487bc003b07ac9e122a306fc
Javier C <javier@javierachavez.com>
2014-04-09 15:09:48 -0600

    updated seed data with new cols

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,13 @@ app = App.create({searchterm: "facebook",
                   package: "com.facebook.katana",
                   name: "Facebook",
                   rating: 3.9,
+                  developer: 'Facebook',
+                  lastupdate: 'March 21, 2014',
+                  installs: 500000000,
+                  website: 'https://www.facebook.com/FacebookMobile',
+                  email: 'android-support@fb.com',
+                  privacypolicy: 'https://www.facebook.com/about/privacy/',
+                  ratings: 14902908,
                   description: 'Keeping up with friends is faster than ever.\nSee what friends are up to\nShare updates, photos and videos\net notified when friends like and comment on your posts\nText, chat and have group conversations\nPlay games and use your favorite apps\nNow you can get early access to the next version of Facebook for Android by becoming a beta tester. Learn how to sign up, givefeedback and leave the program in our Help Center: http://on.fb.me/133NwuP\nProblems downloading or installing the app? See http://bit.ly/GPDownload1\nStill need help? Please tell us more about the issue. http://bit.ly/invalidpackage\nFacebook is only available for users age 13 and over.\nTerms of Service: http://m.facebook.com/terms.php.'})
 
 app.permissions << Permission.find(1)
