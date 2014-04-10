74f73bdaaf075c9c7d7005c0f3cadfcbba0db451
Javier C <javier@javierachavez.com>
2014-04-09 23:57:16 -0600

    added more apps and comments to DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@
 end
 
 
-app = App.create({searchterm: "facebook",
+app2 = App.create({searchterm: "facebook",
                   package: "com.facebook.katana",
                   name: "Facebook",
                   rating: 3.9,
@@ -28,9 +28,46 @@ app = App.create({searchterm: "facebook",
                   ratings: 14902908,
                   description: 'Keeping up with friends is faster than ever.\nSee what friends are up to\nShare updates, photos and videos\net notified when friends like and comment on your posts\nText, chat and have group conversations\nPlay games and use your favorite apps\nNow you can get early access to the next version of Facebook for Android by becoming a beta tester. Learn how to sign up, givefeedback and leave the program in our Help Center: http://on.fb.me/133NwuP\nProblems downloading or installing the app? See http://bit.ly/GPDownload1\nStill need help? Please tell us more about the issue. http://bit.ly/invalidpackage\nFacebook is only available for users age 13 and over.\nTerms of Service: http://m.facebook.com/terms.php.'})
 
-app.permissions << Permission.find(1)
+app3 = App.create({searchterm: "facebook",
+                  package: "com.facebook.orca",
+                  name: "Facebook Messenger",
+                  rating: 4.2,
+                  developer: 'Facebook',
+                  lastupdate: 'April 9, 2014',
+                  installs: 500000000,
+                  website: 'https://www.facebook.com/FacebookMobile',
+                  email: 'android-support@fb.com',
+                  privacypolicy: 'https://www.facebook.com/about/privacy/',
+                  ratings: 4003474,
+                  description: 'Keeping up with friends is faster than ever.\nSee what friends are up to\nShare updates, photos and videos\net notified when friends like and comment on your posts\nText, chat and have group conversations\nPlay games and use your favorite apps\nNow you can get early access to the next version of Facebook for Android by becoming a beta tester. Learn how to sign up, givefeedback and leave the program in our Help Center: http://on.fb.me/133NwuP\nProblems downloading or installing the app? See http://bit.ly/GPDownload1\nStill need help? Please tell us more about the issue. http://bit.ly/invalidpackage\nFacebook is only available for users age 13 and over.\nTerms of Service: http://m.facebook.com/terms.php.'})
 
-comment = Comment.create({author:'rblutionkimmyb',
+app1 = App.create({searchterm: "twitter",
+                  package: "com.handmark.tweetcaster",
+                  name: "TweetCaster for Twitter",
+                  rating: 4.4,
+                  developer: 'OneLouder Apps',
+                  lastupdate: 'April 2, 2014',
+                  installs: 500000000,
+                  website: 'http://www.onelouder.com',
+                  email: 'support@onelouder.com',
+                  privacypolicy: 'https://tweetcaster.com/privacy',
+                  ratings: 475805,
+                  description: 'Keeping up with friends is faster than ever.\nSee what friends are up to\nShare updates, photos and videos\net notified when friends like and comment on your posts\nText, chat and have group conversations\nPlay games and use your favorite apps\nNow you can get early access to the next version of Facebook for Android by becoming a beta tester. Learn how to sign up, givefeedback and leave the program in our Help Center: http://on.fb.me/133NwuP\nProblems downloading or installing the app? See http://bit.ly/GPDownload1\nStill need help? Please tell us more about the issue. http://bit.ly/invalidpackage\nFacebook is only available for users age 13 and over.\nTerms of Service: http://m.facebook.com/terms.php.'})
+
+
+app1.permissions << Permission.find(1)
+app2.permissions << Permission.find(2)
+app3.permissions << Permission.find(1)
+
+comment2 = Comment.create({author:'rblutionkimmyb',
                           content: 'Where\'d the notification bar go?!? What the hell Facebook after the last update it tells me i have notifications but I open the app and there\'s no way to check them please fix not very convenient when I can\'t use my phone'})
 
-app.comments << comment
+comment3 = Comment.create({author:'Keltin Hamilton',
+                          content: 'Great app! It\s a nice, reliable app and I personally have no issues or complaints at all other than not being able to share videos.'})
+
+comment1 = Comment.create({author:'Kevin Queiroz',
+                          content: 'Not too bad More options than the official app, don\'t like how it doesn\'t show who favorited or sometimes even retweeted your tweets though.'})
+
+app2.comments << comment2
+app3.comments << comment3
+app1.comments << comment1
