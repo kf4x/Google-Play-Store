dfa3fd5e1263c739c8a43b7dc5b61c63b82863d4
Javier C <javier@javierachavez.com>
2014-04-17 22:55:24 -0600

    refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 GooglePlayStore::Application.routes.draw do
-  root :to => 'apps#list', as: 'show_all'
   # get '/', to: 'apps#list', as: 'show_all'
-
   get 'apps/details' => 'apps#show', :constraints => { :id => /[\w+\.]+/ }, as: 'get_app'
 
   # apps/developer/Facebook
@@ -9,64 +7,9 @@ GooglePlayStore::Application.routes.draw do
 
   # apps/developer?dev=
   get 'apps/developer' => 'apps#list_by_dev'
+
   # apps/search?q=
-  get 'apps/search'=> 'apps#list_by_search'
+  get 'apps/search' => 'apps#list_by_search'
 
-  # match '/apps', to: 'apps#show', as: 'list_apps'
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  root :to => 'apps#list', as: 'show_all'
 end
