0b784bb752fdbc0ab5a163e5a690082588aac6bf
Javier C <youracow@gmail.com>
2015-04-06 15:46:01 -0600

    intial modeling

--- google_play/store/models.py
+++ google_play/store/models.py
@@ -2,14 +2,29 @@ from django.db import models
 
 # Create your models here.
 
+
+    
+class Permission(models.Model):
+    
+    name = models.CharField(max_length=512)
+    description = models.TextField()
+    category = models.CharField(max_length=512)
+    
+
 class App(models.Model):
-    pass
 
+    name = models.CharField(max_length=512)
+    description = models.TextField()
+    package = models.CharField(max_length=512)
+    category = models.CharField(max_length=512)
 
-class Permission(model.Model):
-    pass
+    # many to many 
+    permissions = models.ManyToManyField(Permission, related_name='apps')
+    
+class Comment(models.Model):
 
+    user = models.CharField(max_length=512)
+    text = models.TextField()
 
-class Comment(model.Model):
-    pass
-
+    # one to many
+    app = models.ForeignKey(App)
