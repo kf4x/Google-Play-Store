e2e3fd37e28f26b3a5c37fe57b73a1bd7ef43f66
Javier C <youracow@gmail.com>
2015-04-05 20:39:19 -0600

    new app

---
+++ google_play/store/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class App(models.Model):
+    pass
+
+
+class Permission(model.Model):
+    pass
+
+
+class Comment(model.Model):
+    pass
+
