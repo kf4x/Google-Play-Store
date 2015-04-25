394a8d93fabf76b3498541174dc9d259a73fecf4
Javier C <youracow@gmail.com>
2015-04-24 23:27:42 -0600

    Urls to accompy new endpoints. Search url and app detail is working, search view supports url based query params.

--- google_play/store/urls.py
+++ google_play/store/urls.py
@@ -5,8 +5,8 @@ from .views import IndexView, DetailView, ResultsView
 urlpatterns = patterns(
     '',
     url(r'^$', IndexView.as_view(), name='home'),
-    url(r'apps/details*$', DetailView.as_view(), name='app'),
-    url(r'apps/details/(?P<slug>[\w-]+)$', DetailView.as_view(), name='app'),
+    url(r'apps/details/(?P<package>[^/]+)*$', DetailView.as_view(), name='app_details'),
+    #url(r'apps/details/(?P<package>[\w])$', DetailView.as_view(), name='app'),
     url(r'search/*$', ResultsView.as_view(), name='search'),
     
 )
--- google_play/store/views.py
+++ google_play/store/views.py
@@ -1,27 +1,52 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.views import generic
-from .models import AndroidApplication
+from .models import AndroidApplication, hamming_stats
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext, loader
+
+
 
 class IndexView(generic.TemplateView):
     template_name = 'store/index.djhtml'
 
     
-class DetailView(generic.DetailView):
+class DetailView(generic.View):
     model = AndroidApplication
     template_name = 'store/detail.djhtml'
 
     def get(self, request, *args, **kwargs):
+        try:
+            pkg  = self.kwargs["package"]
+            self.object= AndroidApplication.objects.get(package=pkg)
+            self.object.description = self.object.description
+            self.object.percentrating = (self.object.rating/5.0)*100
+            
+        except Exception, e:
+            print e
 
-        self.object = {'app':'name'}
-        context = self.get_context_data(object=self.object)
+        t = loader.get_template('store/detail.djhtml')
+        c = RequestContext(request, self.object)
+        return render_to_response(
+            'store/detail.djhtml',
+            {'object':self.object},
+            context_instance=RequestContext(request)
+        )
+                          
 
-        return self.render_to_response(context)
 
-    
 class ResultsView(generic.ListView):
-
     template_name = 'store/results.djhtml'
-
+    
     def get_queryset(self):
-        """All apps"""
-        return AndroidApplication.objects.all()
+        cont = self.request.GET.get('q')
+        return AndroidApplication.objects.filter(name__contains=cont)[:20]
+    
+    def get_context_data(self, **kwargs):
+        context = super(ResultsView, self).get_context_data(**kwargs)
+        # add data to context about this result
+        stats = hamming_stats(context['object_list'])
+        context['hamming'] = stats
+        return context
