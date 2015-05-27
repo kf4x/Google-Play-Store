9daa7a751af1e17293ab863e38f52c7c3877b6cd
Javier C <youracow@gmail.com>
2015-05-27 12:32:46 -0600

    Added new endpoint that returns apps for given dev

--- google_play/store/urls.py
+++ google_play/store/urls.py
@@ -1,12 +1,13 @@
 from __future__ import unicode_literals
 from django.conf.urls import patterns, include, url
-from .views import IndexView, DetailView, ResultsView
+from .views import IndexView, AppDetailView, SearchResultsView, DevDetailView
 
 urlpatterns = patterns(
     '',
     url(r'^$', IndexView.as_view(), name='home'),
-    url(r'apps/details/(?P<package>[^/]+)*$', DetailView.as_view(), name='app_details'),
+    url(r'apps/details/(?P<package>[^/]+)*$', AppDetailView.as_view(), name='app_details'),
+    url(r'apps/developer/(?P<dev>[^/]+)*$', DevDetailView.as_view(), name='more_by_dev'),
     #url(r'apps/details/(?P<package>[\w])$', DetailView.as_view(), name='app'),
-    url(r'search/*$', ResultsView.as_view(), name='search'),
+    url(r'search/*$', SearchResultsView.as_view(), name='search'),
     
 )
