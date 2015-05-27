b8612e77b9914b7a74e85a7bc05b830898d81301
Javier C <youracow@gmail.com>
2015-05-27 02:20:59 -0600

    cleaning up file

--- google_play/store/views.py
+++ google_play/store/views.py
@@ -14,7 +14,7 @@ from django.db.models import Q
 class IndexView(generic.TemplateView):
     template_name = 'store/index.djhtml'
 
-    
+
 class DetailView(generic.View):
     model = AndroidApplication
     template_name = 'store/detail.djhtml'
@@ -24,7 +24,7 @@ class DetailView(generic.View):
         pkg  = self.kwargs["package"]
         self.object = AndroidApplication.objects.get(package=pkg)
         #self.object.description = self.object.description
-        
+
         #t = loader.get_template('store/detail.djhtml')
         #c = RequestContext(request)
 
@@ -33,24 +33,24 @@ class DetailView(generic.View):
             more = AndroidApplication.objects.filter(developer=dev)[:10]
         else:
             more = []
-        
+
         return render_to_response(
             'store/detail.djhtml',
             {'object': self.object,
              'more': more},
             context_instance=RequestContext(request)
         )
-                          
+
 
 
 class ResultsView(generic.ListView):
     template_name = 'store/results.djhtml'
-    
+
     def get_queryset(self):
         cont = self.request.GET.get('q')
         return AndroidApplication.objects.filter(
             Q(name__contains=cont) | Q(description__contains=cont))[:40]
-    
+
     def get_context_data(self, **kwargs):
         context = super(ResultsView, self).get_context_data(**kwargs)
         # add data to context about this result
